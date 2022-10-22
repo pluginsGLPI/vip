f9c8f09902db56281eb1135ed41f557f541e11ef
tsmr <thetsmr@gmail.com>
2022-10-22 11:56:17 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_VIP_VERSION', '1.8.1');
+define('PLUGIN_VIP_VERSION', '1.8.2');
 
 if (!defined("PLUGIN_VIP_DIR")) {
    define("PLUGIN_VIP_DIR", Plugin::getPhpDir("vip"));
