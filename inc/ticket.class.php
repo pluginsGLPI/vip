ff2495cc665e46883eecd562120fd02354f21d9e
Thetsmr <thetsmr@gmail.com>
2024-01-25 12:23:55 +0100

    Fix color

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -28,153 +28,158 @@
  */
 
 if (!defined('GLPI_ROOT')) {
-   die("Sorry. You can't access directly to this file");
+    die("Sorry. You can't access directly to this file");
 }
 
-class PluginVipTicket extends CommonDBTM {
+class PluginVipTicket extends CommonDBTM
+{
+    public static $types = ['Ticket', 'Printer', 'Computer'];
 
-   static $types = ['Ticket', 'Printer', 'Computer'];
+    /**
+     * @param $uid
+     *
+     * @return bool
+     */
+    public static function isUserVip($uid)
+    {
+        global $DB;
 
-   /**
-    * @param $uid
-    *
-    * @return bool
-    */
-   static function isUserVip($uid) {
-      global $DB;
-
-      if ($uid) {
-         $vipquery = "SELECT `glpi_plugin_vip_groups`.`id`
+        if ($uid) {
+            $vipquery = "SELECT `glpi_plugin_vip_groups`.`id`
                    FROM `glpi_groups_users`
                    LEFT JOIN `glpi_plugin_vip_groups`
                      ON `glpi_plugin_vip_groups`.`id` = `glpi_groups_users`.`groups_id`
                    WHERE `glpi_plugin_vip_groups`.`isvip` = 1
                    AND `glpi_groups_users`.`users_id` = " . $uid;
 
-         $result = $DB->query($vipquery);
-         $nb     = $DB->numrows($result);
-         if ($nb > 0) {
-            while ($uids = $DB->fetchArray($result)) {
-               return $uids['id'];
+            $result = $DB->query($vipquery);
+            $nb     = $DB->numrows($result);
+            if ($nb > 0) {
+                while ($uids = $DB->fetchArray($result)) {
+                    return $uids['id'];
+                }
             }
-         }
-      }
+        }
 
-      return false;
-   }
+        return false;
+    }
 
-   /**
-    * @param $entities
-    *
-    * @return array
-    */
-   static function getUserVipList($entities) {
-      global $DB;
+    /**
+     * @param $entities
+     *
+     * @return array
+     */
+    public static function getUserVipList($entities)
+    {
+        global $DB;
 
-      $vip      = [];
-      $vipquery = "SELECT `glpi_groups_users`.`users_id`
+        $vip      = [];
+        $vipquery = "SELECT `glpi_groups_users`.`users_id`
                    FROM `glpi_groups_users`
                    LEFT JOIN `glpi_plugin_vip_groups`
                      ON `glpi_plugin_vip_groups`.`id` = `glpi_groups_users`.`groups_id`
                    WHERE `glpi_plugin_vip_groups`.`isvip` = 1";
 
-      $result = $DB->query($vipquery);
-      $nb     = $DB->numrows($result);
-      if ($nb > 0) {
-         while ($uids = $DB->fetchArray($result)) {
-            $vip[] = $uids['users_id'];
-         }
-      }
-      return $vip;
-   }
+        $result = $DB->query($vipquery);
+        $nb     = $DB->numrows($result);
+        if ($nb > 0) {
+            while ($uids = $DB->fetchArray($result)) {
+                $vip[] = $uids['users_id'];
+            }
+        }
+        return $vip;
+    }
 
-   /**
-    * @param $ticketid
-    *
-    * @return bool
-    */
-   static function isTicketVip($ticketid) {
-      global $DB;
+    /**
+     * @param $ticketid
+     *
+     * @return bool
+     */
+    public static function isTicketVip($ticketid)
+    {
+        global $DB;
 
-      if ($ticketid > 0) {
-         $userquery  = "SELECT `users_id`
+        if ($ticketid > 0) {
+            $userquery  = "SELECT `users_id`
                         FROM `glpi_tickets_users`
                         WHERE `type` = " . CommonITILActor::REQUESTER . "
                         AND `tickets_id` = " . $ticketid;
-         $userresult = $DB->query($userquery);
-         $nb     = $DB->numrows($userresult);
-         if ($nb > 0) {
-            while ($uids = $DB->fetchArray($userresult)) {
-               $isuservip = self::isUserVip($uids['users_id']);
-               if ($isuservip > 0) {
-                  return $isuservip;
-               }
-
+            $userresult = $DB->query($userquery);
+            $nb     = $DB->numrows($userresult);
+            if ($nb > 0) {
+                while ($uids = $DB->fetchArray($userresult)) {
+                    $isuservip = self::isUserVip($uids['users_id']);
+                    if ($isuservip > 0) {
+                        return $isuservip;
+                    }
+                }
             }
-         }
-      }
-      return false;
-   }
+        }
+        return false;
+    }
 
-   /**
-    * @param $printers_id
-    *
-    * @return bool
-    */
-   static function isPrinterVip($printers_id) {
+    /**
+     * @param $printers_id
+     *
+     * @return bool
+     */
+    public static function isPrinterVip($printers_id)
+    {
+        $printer = new Printer();
+        $printer->getFromDB($printers_id);
+        return self::isUserVip($printer->getField('users_id'));
+    }
 
-      $printer = new Printer();
-      $printer->getFromDB($printers_id);
-      return self::isUserVip($printer->getField('users_id'));
-   }
+    /**
+     * @param $computers_id
+     *
+     * @return bool
+     */
+    public static function isComputerVip($computers_id)
+    {
+        $computer = new Computer();
+        $computer->getFromDB($computers_id);
+        return self::isUserVip($computer->getField('users_id'));
+    }
 
-   /**
-    * @param $computers_id
-    *
-    * @return bool
-    */
-   static function isComputerVip($computers_id) {
+    /**
+     * @param $params
+     *
+     * @return void
+     */
+    public static function showVIPInfos($params)
+    {
+        $item = $params['item'];
 
-      $computer = new Computer();
-      $computer->getFromDB($computers_id);
-      return self::isUserVip($computer->getField('users_id'));
-   }
-
-   /**
-    * @param $params
-    *
-    * @return void
-    */
-   public static function showVIPInfos($params) {
-      $item = $params['item'];
-
-      if ($item != null && in_array($item->getType(), self::$types)) {
-         if ($item->getType() == 'Ticket') {
-            if ($id = self::isTicketVip($item->getID())) {
-               $name = PluginVipGroup::getVipName($id);
-               $icon = PluginVipGroup::getVipIcon($id);
-               echo "<div class='alert alert-important alert-warning center '>";
-               echo "<i class='fas $icon fa-2x' title=\"$name\" style='font-family:\"Font Awesome 5 Free\", \"Font Awesome 5 Brands\";'></i>&nbsp;";
-               echo sprintf(__('%1$s %2$s'), __('This ticket concerns at least one', 'vip'), $name);
-               echo "</div>";
+        if ($item != null && in_array($item->getType(), self::$types)) {
+            if ($item->getType() == 'Ticket') {
+                if ($id = self::isTicketVip($item->getID())) {
+                    $name = PluginVipGroup::getVipName($id);
+                    $icon = PluginVipGroup::getVipIcon($id);
+                    $color = PluginVipGroup::getVipColor($id);
+                    echo "<div class='alert alert-important alert-warning center' style='background-color: $color'>";
+                    echo "<i class='fas $icon fa-2x' title=\"$name\" style='font-family:\"Font Awesome 5 Free\", \"Font Awesome 5 Brands\";'></i>&nbsp;";
+                    echo sprintf(__('%1$s %2$s'), __('This ticket concerns at least one', 'vip'), $name);
+                    echo "</div>";
+                }
+            } else {
+                if ($id = self::isUserVip($item->getField('users_id'))) {
+                    $color = PluginVipGroup::getVipColor($id);
+                    echo "<div class='alert alert-important alert-warning center' style='background-color: $color'>";
+                    if ($item->getType() == 'Computer') {
+                        $name = PluginVipGroup::getVipName($id);
+                        $icon = PluginVipGroup::getVipIcon($id);
+                        echo "<i class='fas $icon fa-2x' title=\"$name\" style='font-family:\"Font Awesome 5 Free\", \"Font Awesome 5 Brands\";'></i>&nbsp;";
+                        echo sprintf(__('%1$s %2$s'), __('This computer is used by a', 'vip'), $name);
+                    } elseif ($item->getType() == 'Printer') {
+                        $name = PluginVipGroup::getVipName($id);
+                        $icon = PluginVipGroup::getVipIcon($id);
+                        echo "<i class='fas $icon fa-2x' title=\"$name\" style='font-family:\"Font Awesome 5 Free\", \"Font Awesome 5 Brands\";'></i>&nbsp;";
+                        echo sprintf(__('%1$s %2$s'), __('This printer is used by a', 'vip'), $name);
+                    }
+                    echo "</div>";
+                }
             }
-         } else {
-            if ($id = self::isUserVip($item->getField('users_id'))) {
-               echo "<div class='alert alert-important alert-warning center '>";
-               if ($item->getType() == 'Computer') {
-                  $name = PluginVipGroup::getVipName($id);
-                  $icon = PluginVipGroup::getVipIcon($id);
-                  echo "<i class='fas $icon fa-2x' title=\"$name\" style='font-family:\"Font Awesome 5 Free\", \"Font Awesome 5 Brands\";'></i>&nbsp;";
-                  echo sprintf(__('%1$s %2$s'), __('This computer is used by a', 'vip'), $name);
-               } else if ($item->getType() == 'Printer') {
-                  $name = PluginVipGroup::getVipName($id);
-                  $icon = PluginVipGroup::getVipIcon($id);
-                  echo "<i class='fas $icon fa-2x' title=\"$name\" style='font-family:\"Font Awesome 5 Free\", \"Font Awesome 5 Brands\";'></i>&nbsp;";
-                  echo sprintf(__('%1$s %2$s'), __('This printer is used by a', 'vip'), $name);
-               }
-               echo "</div>";
-            }
-         }
-      }
-   }
+        }
+    }
 }
